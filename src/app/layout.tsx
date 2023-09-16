b7e8265eaa3365240d8e6b3f405cffc1354baebb
unknown <anikatahsinmeem@gmail.com>
2023-09-16 17:46:07 +0600

    header logo part done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import HeaderTop from '@/components/HeaderTop'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import HeaderMain from '@/components/HeaderMain'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={inter.className}>
         
         <HeaderTop />
+        <HeaderMain />
         {children}</body>
     </html>
   )
